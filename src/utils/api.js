c9596646386bd1e4702dcb26fc17692d615193a5
BlueBearrii <thananan.math.acs@gmail.com>
2021-09-24 19:10:09 +0700

    utils: Utility folder

---
+++ src/utils/api.js
@@ -0,0 +1,5 @@
+module.exports = {
+    root : "https://candidate.neversitup.com/todo",
+    login : "/users/auth",
+    getAll: "/todos/"
+}
---
+++ src/utils/themes.js
@@ -0,0 +1,4 @@
+module.exports = {
+    primaryColor: "#22577A",
+    secondaryColor : "#38A3A5",
+};
