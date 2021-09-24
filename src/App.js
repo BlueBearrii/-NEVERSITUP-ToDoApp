3c362a4a19eb3b9970989f5aa56f7c9d2ed4a24e
BlueBearrii <thananan.math.acs@gmail.com>
2021-09-24 19:12:37 +0700

    config: Config provider for redux state management

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import TodoDashboard from './components/app/TodoDashboard';
+import Authentication from './components/authentication/Authentication';
 
-function App() {
+import './App.css'
+
+function App({ authentication }) {
+
+  console.log(authentication)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      {authentication.token ? <TodoDashboard /> : <Authentication />}
+    </AppContainer>
   );
 }
 
-export default App;
+const AppContainer = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
+
+  max-width: 600px;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const mapStateToProps = (state) => {
+  return state
+}
+
+export default connect(mapStateToProps)(App);
